82ba34a1b336ed929d65632045ab4a1180f8ff62
Ihsan Berahim <ihsanberahim@gmail.com>
2023-05-10 22:34:15 +0800

    save commands i'm using

--- run.sh
+++ run.sh
@@ -2,10 +2,13 @@ python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
 
-python pdftotext.py hadis-40.pdf hadis-40.txt
+python pdftotext.py document.pdf document.txt
 
 python download_model.py 117M
 
-PYTHONPATH=src python encode.py hadis-40.txt hadis-40.npz
+PYTHONPATH=src python encode.py document.txt document.npz
 
-PYTHONPATH=src python ./train.py --dataset hadis-40.npz --model_name 117M --run_name=v1
+PYTHONPATH=src python ./train.py --dataset document.npz --model_name 117M --run_name=brian_hocklai
+PYTHONPATH=src python ./train.py --dataset document.npz --model_name 117M --run_name=brian_esan
+
+python ./src/interactive_conditional_samples.py --temperature 0.8 --top_k 40 --model_name v1
